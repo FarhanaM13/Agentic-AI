8d8a87d8808394d53b5bed0ef0d81e9160ce4e84
FarhanaM13 <240201601021@crescent.education>
2026-09-09 11:38:46 +0530

    Fix Flask app initialization syntax

--- App/__init__.py
+++ App/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 from App.youtube import youtube_bp
 def create_App():
-app = Flask(**name**)
+app = Flask(name)
 app.register_blueprint(
     youtube_bp,
     url_prefix="/youtube"
