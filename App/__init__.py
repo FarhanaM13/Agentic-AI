0bd61ec778bb19b38e0d78f3c127cab817c0a490
FarhanaM13 <240201601021@crescent.education>
2026-09-09 11:14:01 +0530

    Refactor create_App function to use 'app' variable

--- App/__init__.py
+++ App/__init__.py
@@ -1,23 +1,24 @@
 from flask import Flask, render_template
-from App.youtube import youtube_bp
 
+from App.youtube import youtube_bp
 
 def create_App():
 
-    App = Flask(__name__)
+```
+app = Flask(__name__)
 
-    App.register_blueprint(
-        youtube_bp,
-        url_prefix="/youtube"
-    )
+app.register_blueprint(
+    youtube_bp,
+    url_prefix="/youtube"
+)
 
-    @App.route("/")
-    def home():
-        return render_template("index.html")
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-    @App.route("/html")
-    def html():
-        return render_template("index.html")
+@app.route("/html")
+def html():
+    return render_template("index.html")
 
-    return App
-
+return app
+```
